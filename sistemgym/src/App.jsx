77f4ad680fd48b79f354ba64c64370c1dd99da91
niveyrojulian <niveyrojulian5@gmail.com>
2024-07-05 15:25:43 -0300

    first commit

---
+++ sistemgym/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-screen">
+      <div className="flex">
+        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
