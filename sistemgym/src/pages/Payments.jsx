b71eeb6736d32cf65a52b7593ecb3cf3923b3017
niveyrojulian <niveyrojulian5@gmail.com>
2024-07-09 18:15:16 -0300

    Todos los cambios que hice hasta ahora

---
+++ sistemgym/src/pages/Payments.jsx
@@ -0,0 +1,11 @@
+import SelectPayments from '../components/Payment/SelectPayments'
+
+const Payments = () => {
+  return (
+    <div>
+        <SelectPayments />
+    </div>
+  )
+}
+
+export default Payments
